13dbb3325f7c519b0e0e52a88631c066bcb3edf2
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-10 20:26:23 -0300

    criando um componente

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import React from 'react';
+
 const Bemvindo=(props)=>{
-return(
-  <div>
-    <h2>ola! moçada:{props.nome})</h2>
-  </div>
-);
+  return(
+    <div>
+      <h2>Bemmm vindo:{props.nome}</h2>
+      <h2>Minha idade é:{props.idade}</h2>
+      <h2>Minha profissão é:{props.cargo}</h2>
+
+    </div>
+  )
 }
+
 
 
 export default function App(){
   return(
     <div>
       <h1>Bem Vindo ao Sistema</h1>
-      <h2>@SujeitoProgramador:)</h2>
-      <Bemvindo nome="Ariosvaldo"/>
+      <Bemvindo nome="Ari" idade="26"cargo="desenvolvedor Jr"/>
+      <Bemvindo nome="Sara" idade="33" cargo="desenvolvedor Master" />
+      <Bemvindo nome="Rafa" idade="12" cargo="desenvolvedor Senior"/>
+
+
     </div>
   )
 }
