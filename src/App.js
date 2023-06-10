17cd29f4246556adb0a155a7da2248b4a5836a0c
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-10 17:28:13 -0300

    trabalhando com props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+const Bemvindo=(props)=>{
+return(
+  <div>
+    <h2>ola! moçada:{props.nome})</h2>
+  </div>
+);
+}
+
 
 export default function App(){
   return(
     <div>
       <h1>Bem Vindo ao Sistema</h1>
       <h2>@SujeitoProgramador:)</h2>
+      <Bemvindo nome="Ariosvaldo"/>
     </div>
   )
 }
