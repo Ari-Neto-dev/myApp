615cd498eb9953bacbecb604f56fce1a9f6b7507
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-11 15:40:06 -0300

    passando props como paramentro

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const Bemvindo=(props)=>{
+const Cadastro=(props)=>{
   return(
     <div>
-      <h2>Bemmm vindo:{props.nome}</h2>
-      <h2>Minha idade é:{props.idade}</h2>
-      <h2>Minha profissão é:{props.cargo}</h2>
-
+      <strong>
+        <h1>Gostaria de me apresentar, sou o {props.nome} minha profissão é {props.atividade} e tenho muita experência com{props.biblioteca} </h1>
+      </strong>
     </div>
   )
 }
 
 
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <div>
       <h1>Bem Vindo ao Sistema</h1>
-      <Bemvindo nome="Ari" idade="26"cargo="desenvolvedor Jr"/>
-      <Bemvindo nome="Sara" idade="33" cargo="desenvolvedor Master" />
-      <Bemvindo nome="Rafa" idade="12" cargo="desenvolvedor Senior"/>
-
-
+      <Cadastro nome="Ari" atividade="desenvolvedor" biblioteca=" REACT"/>
+    
     </div>
-  )
+  );
 }
