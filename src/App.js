ceeb0ede53177abeba07a08e3e1b374b6b539b2d
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-16 12:11:16 -0300

    ciclo de vida

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React,{Component} from "react";
+
+
+
+
+
+import React, {Component}from 'react';
 
 class App extends Component{
+
   constructor(props){
     super(props);
     this.state={
-      hora: ' 00.00.00'
+      hora:' 00:00:00'
     };
   }
 
   componentDidMount(){
     setInterval(()=>{
       this.setState({hora: new Date().toLocaleTimeString()})
+
     },1000);
+
   }
+
+  componentDidUpdate(){
+    console.log('Atualizou!!!');
+
+  }
+  // shouldComponentUpdate(){
+    
+  // }
+
 
   render(){
     return(
       <div>
-        <h1>Meu Projeto{ this.state.hora})</h1>
+        <h1>Meu Projeto{this.state.hora}</h1>
       </div>
     )
   }
 }
-
-
-
 export default App;
