0715fa840abeee3e19f98a20b0abcd5167951e0a
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-21 15:27:01 -0300

    Eventos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, {Component} from "react";
+import React,{Component} from "react";
 import Membro from './componentes/Membro'
 class App extends Component{
   render(){
     return(
       <div>
-        olá Mundo !
-        <Membro/>
+        <h1><Membro nome=" Visitante" /></h1>
+      
       </div>
     )
-    
   }
 }
-
 export default App;
