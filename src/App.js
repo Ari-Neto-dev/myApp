6179bfe43ca340a6777b33f966a38ee35dcae413
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-21 16:21:00 -0300

    Renderização

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React,{Component} from "react";
-import Membro from './componentes/Membro'
-class App extends Component{
-  render(){
-    return(
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: false,
+    };
+
+    this.sair = this.sair.bind(this);
+    this.entrar = this.entrar.bind(this);
+  }
+  sair(){
+    this.setState({status: false});
+  }
+
+  entrar(){
+    this.setState({status:true});
+  }
+
+  render() {
+    return (
       <div>
-        <h1><Membro nome=" Visitante" /></h1>
-      
+       {this.state.status ?
+       <div>
+        <h2>Bem vindo ao Sistema</h2>
+        <button onClick={this.sair}>SAIR</button>
+       </div>:
+       <div>
+        <h2>Olá visitante faça o login</h2>
+        <button onClick={this.entrar}>Entrar no Sistema</button>
+       </div>
+      }
       </div>
-    )
+    );
   }
 }
+
 export default App;
