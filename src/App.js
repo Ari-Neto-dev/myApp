fe434de30dc0202133ebada646330bf8056e8a0d
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-22 12:25:41 -0300

    formulário1

--- src/App.js
+++ src/App.js
@@ -3,16 +3,61 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email:'',
+      senha:'',
+      sexo:'masculino'
+    }
+
+    this.trocaEmail=this.trocaEmail.bind(this);
+    this.trocaSenha=this.trocaSenha.bind(this);
+    this.trocaSexo=this.trocaSexo.bind(this);
+  }
+
+  trocaEmail(e){
+    let valorDigitado = e.target.value;
+    this.setState({email:valorDigitado});
+  }
+
+  trocaSenha(e){
+    let valorDigitado=e.target.value;
+    this.setState({senha:valorDigitado});
+  }
+
+  trocaSexo(e){
+    let valorDigitado = e.target.value
+    this.setState({sexo:valorDigitado})
   }
 
   render() {
     return(
        <div>
+        <h2>Login</h2>
         email:
-        <input type="email" name="email" value="teste@teste.com"/><br/>
+        <input type="email" name="email" value={this.state.email}
+        onChange={this.trocaEmail}/><br/>
         Senha:
-        <input type="password" name="senha" value="123456" />
+        <input type="password" name="senha" value={this.state.senha}
+        onChange={this.trocaSenha}/><br/>
+
+        Sexo:
+        <select name="sexo" value={this.state.sexo} onChange={this.trocaSexo}>
+          <option value="masculino" >Masculino</option>
+          <option value="feminino">Feminino</option>
+        </select>
+
+        <div>
+          <h3>{this.state.email}</h3>
+          <h3>{this.state.senha}</h3>
+          <h3>{this.state.sexo}</h3>
+      
+        </div>
+
+
+
+
+
+
 
        </div>
     );
