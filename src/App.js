796031c705f872dcec52003250a1fd80b8675258
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-22 10:37:51 -0300

    formulario

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
 import React, { Component } from "react";
-import Feed from "./Component/Feed";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      feed:[
-        {id:1, username:'Ariosvaldo',curtidas:'10',comentarios:2},
-        {id:2, username:'Lucas',curtidas:'120',comentarios:24},
-        {id:3, username:'Amanda',curtidas:'30',comentarios:12},
-        {id:4, username:'Silvia',curtidas:'1',comentarios:0},
-        {id:5, username:'Rosa',curtidas:'100',comentarios:8},
-        {id:6, username:'Maria',curtidas:'99',comentarios:10},
-
-
-        
-      ]
-
-    };
+    this.state = {};
   }
 
-  render(){
+  render() {
     return(
-      <div>
-        {this.state.feed.map((item)=>{
-          return(
-           <Feed id={item.id} username={item.username} curtidas={item.curtidas}
-           comentarios={item.comentarios}/>
-         
-          );
-        })}
+       <div>
+        email:
+        <input type="email" name="email" value="teste@teste.com"/><br/>
+        Senha:
+        <input type="password" name="senha" value="123456" />
 
-      </div>
+       </div>
     );
   }
 }
-
 export default App;
